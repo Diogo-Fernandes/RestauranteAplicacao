7d97ff913b7d16fdf703077e655a14fed014bdbf
saravdp <39441234+saravdp@users.noreply.github.com>
2019-12-14 16:32:30 +0000

    Add files via upload

--- src/store/user.js
+++ src/store/user.js
@@ -33,23 +33,55 @@ const state={
             id_tipoUser:2
           }]
 }
-const actions={
+const mutations={
 
+  login(context, { name, pass }) {
+    alert(JSON.stringify(this.state.state.users))
 
-login(username,password) {
-    alert(username )
-    alert(password)
-  /*  for (var user in this.state.users){
-      if (user.name== username && user.password ==password){
-        alert("Login efetuado com sucesso");
-
+    var loggedIn = false
+    var users = []
+    var userId= null
+    users = this.state.state.users
+    for (var user in users) {
+      if (users[user] != null) {
+        if (JSON.stringify(users[user].username) == JSON.stringify({ name }.name)) {
+          if (users[user].password == { pass }.pass) {
+            loggedIn = true
+            userId = users[user].id
+          }
+        }
       }
-    }*/
-}
+    }
+    if (loggedIn) {
+      alert("Login efetuado com sucesso");
+      alert(this.getters.getlastID)
+      localStorage.setItem("token", userId)
+      router.push("/usersList")
+    }else if(!loggedIn){
+      alert("dados incorretos")
+      router.go()      }
+    
+  },
+  register(context,{data}){
+    localStorage.clear()
+    const newId=this.getters.getlastID
+
+    alert(JSON.stringify(data.data_nascimento))
+    let newUser={
+      id: newId,
+      nome: data.nome,
+      sobrenome: data.sobrenome,
+      data_nascimento: data.data_nascimento,
+      username: data.username,
+      password: data.password,
+      id_tipoUser:data.id_tipoUser
+    }
+    this.state.users.push(newUser)
+    alert(JSON.stringify(this.state.users))
+  }
 }
 export const users = {
     namespaced: true,
     state,
-    actions,
-    //mutations
+    mutations
 };
